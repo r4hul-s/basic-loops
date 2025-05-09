57e5a6d18aff6274aec0d24e33ace0eae25eecb3
rahul <rahullodha761@gmail.com>
2025-05-09 12:18:18 +0530

    committed

--- table.py
+++ table.py
@@ -5,3 +5,5 @@ num = int(input("Enter a number to print its table: "))
 print(f"\nMultiplication Table of {num}:")
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")
+
+# changes made in file pushed to github
