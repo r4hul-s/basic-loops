00d2c6e92c32ce1ce995acc8136ca77e2edeb427
rahul <rahullodha761@gmail.com>
2025-05-13 21:46:02 +0530

    file committed

---
+++ list.py
@@ -0,0 +1,6 @@
+#basic list operations "create, add, remove, change, access"
+#items in the lists always starts with index 0
+list_items = ["rahul", "tj", "vivek", "mk", "sid"] #list items always entered in ""
+list_items.append("mv") #append is used to add the item in the end
+list_items.insert(2,"sarvi") #insert new value at the specific index 
+print(list_items)
